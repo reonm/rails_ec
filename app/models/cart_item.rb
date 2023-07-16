509e930b0b97f37ca8ca39be6b32be26f512b576
reonm <songcunreon@gmail.com>
2023-07-16 17:16:16 +0900

    rubocopの修正3

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+# CartItem class represents an item in a shopping cart.
 class CartItem < ApplicationRecord
   belongs_to :item
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
 end

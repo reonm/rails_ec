354a8e262f332130dfabf5485341ce49f039708f
reonm <songcunreon@gmail.com>
2023-07-23 18:04:47 +0900

    rubocop修正6

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,7 +3,6 @@
 # This class is responsible for handling cart items. It allows for adding,
 # removing and updating items within a user's shopping cart.
 # CartItemsController handles actions related to cart items.
-
 class CartItemsController < ApplicationController
   def show
     @cart = session[:cart] || {}
--- db/migrate/20230721062956_add_price_to_items.rb
+++ db/migrate/20230721062956_add_price_to_items.rb
@@ -1,3 +1,5 @@
+# Migration to add price to items table.
+# frozen_string_literal: true
 class AddPriceToItems < ActiveRecord::Migration[7.0]
   def change
     add_column :items, :price, :integer
